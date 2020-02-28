f5b3a6a00751f167cc74d5ae3c2473bdc2e42bc1
Fábio Costa <fabiodanielmc@gmail.com>
2020-02-28 17:53:02 +0100

    done

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ var students = [
     {firstname: "Charles",
     lastname: "van den Bergh"
     },
-    {firstname: "Elena",
+    {firstname: "Alena",
     lastname: "Perrone"
     },
     {firstname: "Fábio",
@@ -48,3 +48,67 @@ var students = [
     lastname: "Bukenya"
     }
 ]
+
+let $body = document.querySelector("body");
+
+
+function sayHelloClass(){
+    let $h1 = document.createElement("h1");
+    $h1.innerHTML= "Hello Class";
+    $body.appendChild($h1);
+}
+
+sayHelloClass();
+
+function text() {
+    let $p = document.createElement("p");
+    $p.innerHTML = "Today we're going to solidify our DOM skills!";
+    $body.appendChild($p);
+}
+
+text();
+
+function printStudents(){
+    for(let i = 0; i < students.length ; i++){
+        let $h3 = document.createElement("h3");
+        let firstName = students[i].firstname;
+        let lastName = students[i].lastname;
+        $h3.innerHTML = `${firstName} ${lastName}`;
+        $body.appendChild($h3);
+        let $img = document.createElement("img");
+        let studentName = firstName.toLowerCase();
+        let source = `https://wd-ft-feb.s3.eu-central-1.amazonaws.com/${studentName}.png`
+        $img.setAttribute("src",source);
+        $img.setAttribute("style","width:200px;border:5px solid black");
+        $body.appendChild($img);
+    }
+}
+
+printStudents();
+
+function randomIndex(){
+    return Math.floor(Math.random()*students.length);
+}
+
+function randomStudent(){
+    let rndStudentInd = randomIndex();
+    let $h3 = document.createElement("h3");
+    let firstName = students[rndStudentInd].firstname;
+    let lastName = students[rndStudentInd].lastname;
+    $h3.innerHTML = `${firstName} ${lastName}`;
+    $body.appendChild($h3);
+    let $img = document.createElement("img");
+    let studentName = firstName.toLowerCase();
+    let source = `https://wd-ft-feb.s3.eu-central-1.amazonaws.com/${studentName}.png`
+    $img.setAttribute("src",source);
+    $img.setAttribute("style","width:200px;border:5px solid black");
+    $body.appendChild($img);
+}
+
+
+document.querySelector("button").addEventListener("click",function(){
+    $body.innerHTML= "";
+    sayHelloClass();
+    randomStudent();
+})
+
